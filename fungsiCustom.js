00c205599ec483fd25d8eba2d3620e109cd807be
Sendibayu2210 <nugraha102210@gmail.com>
2022-06-12 21:32:27 +0700

    tambah  kode

--- fungsiCustom.js
+++ fungsiCustom.js
@@ -1,4 +1,6 @@
 // TODO: import module bila dibutuhkan di sini
+// Include fs module
+const fs = require('fs');
 
 // ! JANGAN DIMODIFIKASI
 let file1 = "./data1.json";
@@ -18,7 +20,48 @@ let modifyFile3 = (val) => {
 
 // TODO: Kerjakan bacaData
 // gunakan variabel file1, file2, dan file3
-const bacaData = null;
+const bacaData = () => { 
+
+    let fileSatu = fs.readFile(file1, 'utf-8',(err,data)=>{
+      if(err){
+        console.log(err);
+      }else{
+        let a = JSON.parse(data);
+        let b = a.message.split(" ");
+        let c = b[1].toString();        
+        console.log(c)        
+      }      
+    }) 
+
+    let fileDua =  fs.readFile(file2, 'utf-8',(err,data)=>{
+      if(err){
+        console.log(err);
+      }else{
+        let a = JSON.parse(data);        
+        let b = a.map(function(item) {
+          let c =  item['message'].split(" ");
+          let d = c[1].toString();        
+          console.log(d)        
+        });                               
+      }
+    }) 
+
+    let fileTiga =  fs.readFile(file3, 'utf-8',(err,data)=>{
+      if(err){
+        console.log(err);
+      }else{
+        let  a = JSON.parse(data);                
+        let b = a.map(function(item) {
+          let c =  item['data'];   
+          let d = Object.values(c);            
+          return d;          
+        });
+        let f = b.toString().split(" ");                             
+        console.log(f[1]);
+      }      
+    })           
+}
+
 
 // ! JANGAN DIMODIFIKASI
 module.exports = {
